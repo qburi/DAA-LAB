eafcdfa792657617e6ad978b14bb6298b6d0287b
qburi <divyeshbalu@gmail.com>
2025-12-10 14:10:09 +0530

    implemented MaximumUnitsOnATruckFractionalKnapsackProblem.cpp

--- Lab 1/MaximumUnitsOnATruckFractionalKnapsackProblem.cpp
+++ Lab 1/MaximumUnitsOnATruckFractionalKnapsackProblem.cpp
@@ -2,7 +2,27 @@
 
 using namespace std;
 
+class Solution {
+public:
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        sort(boxTypes.begin(), boxTypes.end(), [](vector<int>& left, vector<int>& right) {
+            return left[1] > right[1];
+        });
 
+        int items = 0;
+
+        for (auto& item: boxTypes) {
+            int numberOfItems = item[0];
+            int numberOfUnits = item[1];
+
+            items += numberOfUnits * (truckSize - numberOfItems >= 0 ? numberOfItems : truckSize);
+            truckSize -= numberOfItems;
+            if (truckSize <= 0) return items;
+        }
+
+        return items;
+    }
+};
 
 int main() {
     return 0;
