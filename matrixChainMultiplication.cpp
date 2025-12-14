d42a786a230bbcd3f747030b17204f538144af82
qburi <divyeshbalu@gmail.com>
2025-12-14 22:59:34 +0530

    added memoization to matrixChainMultiplication.cpp

--- matrixChainMultiplication.cpp
+++ matrixChainMultiplication.cpp
@@ -4,28 +4,30 @@ using namespace std;
 
 class Solution {
 public:
-    int solve(int i, int j, vector<int> &arr) {
+    int solve(int i, int j, vector<int> &arr, vector<vector<int>>& dp) {
         if (i == j) return 0; // 0 operations to multiply a matrix with nothing
+        if (dp[i][j] != -1) return dp[i][j];
         int minimumSteps = INT_MAX;
         for (int k = i; k < j; k++) {
-            int steps = arr[i - 1] * arr[k] * arr[j] + solve(i, k, arr) + solve(k + 1, j, arr);
+            int steps = arr[i - 1] * arr[k] * arr[j] + solve(i, k, arr, dp) + solve(k + 1, j, arr, dp);
             minimumSteps = min(minimumSteps, steps);
         }
-
+        dp[i][j] = minimumSteps;
         return minimumSteps;
     }
 
-    int matrixChainMultiplication(vector<int> &arr, int N) {
-        return solve(1, N - 1, arr);
+    int matrixChainMultiplication(vector<int> &arr) {
+        int n = arr.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return solve(1, n - 1, arr, dp);
     }
 };
 
 int main() {
     vector<int> arr{10, 30, 5, 10};
     Solution* solution = new Solution();
-    int N = arr.size();
     cout << "Minimum number of multiplications: ";
-    cout << solution->matrixChainMultiplication(arr, N) << endl;
+    cout << solution->matrixChainMultiplication(arr) << endl;
 
     delete solution;
     return 0;
