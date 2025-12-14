a0795f11db38abce4f40b1dff37c20ba11ac2b6d
qburi <divyeshbalu@gmail.com>
2025-12-14 23:23:26 +0530

    implemented matrixChainMultiplicationTabulationSolution.cpp

---
+++ matrixChainMultiplicationTabulationSolution.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int matrixChainMultiplication(vector<int>& arr) {
+        int n = arr.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, INT_MAX));
+        for (int i = 1; i < n; i++)
+            dp[i][i] = 0; // base case
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = i + 1; j < n; j++) {
+                for (int k = i; k < j; k++) {
+                    dp[i][j] = min(dp[i][j], arr[i - 1] * arr[k] * arr[j] + dp[i][k] + dp[k + 1][j]);
+                }
+            }
+        }
+
+        return dp[1][n - 1];
+    }
+};
+
+int main() {
+    vector<int> arr{10, 30, 5, 10};
+    Solution* solution = new Solution();
+    cout << "Minimum number of multiplications: ";
+    cout << solution->matrixChainMultiplication(arr) << endl;
+
+    delete solution;
+    return 0;
+}
