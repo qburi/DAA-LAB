aa20cdcb2f39fcb7d2546eafd6a64e8822aa997e
Divyesh <divyeshbalu@gmail.com>
2025-12-27 15:13:58 +0530

    updated N Queens.cpp

--- N Queens.cpp
+++ N Queens.cpp
@@ -4,8 +4,13 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<string>> solveNQueens(int n) {
+    vector<vector<string>> ans;
+    int n;
 
+
+    vector<vector<string>> solveNQueens(int n) {
+        this->ans = vector<vector<string>>(n, vector<string>(n, ""));
+        this->n = n;
     }
 };
 
