84c86555eb918752fd789f2ad2ecff515415546b
qburi <divyeshbalu@gmail.com>
2026-01-09 13:30:58 +0530

    updated KaratsubaFasterIntegerMultiplicationAlgorithm.cpp

--- Lab 1/KaratsubaFasterIntegerMultiplicationAlgorithm.cpp
+++ Lab 1/KaratsubaFasterIntegerMultiplicationAlgorithm.cpp
@@ -6,9 +6,26 @@ using namespace std;
 
 class Solution {
 public:
+    long long multiply(long long x, long long y) {
+        if (x < 10 || y < 10)
+            return x * y;
 
+        long long n = max(to_string(x).length(), to_string(y).length());
+        long long half = n / 2;
+        long long a = x / pow(10, half);
+        long long b = x % (long long) pow(10, half);
+        long long c = y / pow(10, half);
+        long long d = y % (long long) pow(10, half);
+
+        long long ac = multiply(a, c);
+        long long bd = multiply(b, d);
+        long long ad_plus_bc = multiply(a + b, c + d) - ac - bd;
+        return ac * pow(10, 2 * half) + (ad_plus_bc * pow(10, half)) + bd;
+    }
 };
 
 int main() {
+    Solution solution = Solution();
+    cout << solution.multiply(200, 8) << endl;
     return 0;
 }
