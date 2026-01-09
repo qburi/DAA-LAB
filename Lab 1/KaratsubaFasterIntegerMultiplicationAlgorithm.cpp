f179c43b637e586b0ddeb40ca7b4df6ad581e27f
qburi <divyeshbalu@gmail.com>
2026-01-09 13:47:19 +0530

    finished KaratsubaFasterIntegerMultiplicationAlgorithm.cpp

--- Lab 1/KaratsubaFasterIntegerMultiplicationAlgorithm.cpp
+++ Lab 1/KaratsubaFasterIntegerMultiplicationAlgorithm.cpp
@@ -6,26 +6,38 @@ using namespace std;
 
 class Solution {
 public:
+    long long powerOfTen(long long n) {
+        long long res = 1;
+        for (int i = 0; i < n; i++) {
+            res *= 10;
+        }
+        return res;
+    }
+
     long long multiply(long long x, long long y) {
         if (x < 10 || y < 10)
             return x * y;
 
-        long long n = max(to_string(x).length(), to_string(y).length());
+        long long n = max(to_string(abs(x)).length(), to_string(abs(y)).length());
         long long half = n / 2;
-        long long a = x / pow(10, half);
-        long long b = x % (long long) pow(10, half);
-        long long c = y / pow(10, half);
-        long long d = y % (long long) pow(10, half);
+        long long multiplier = powerOfTen(half);
+        long long a = x / multiplier;
+        long long b = x % multiplier;
+        long long c = y / multiplier;
+        long long d = y % multiplier;
 
         long long ac = multiply(a, c);
         long long bd = multiply(b, d);
         long long ad_plus_bc = multiply(a + b, c + d) - ac - bd;
-        return ac * pow(10, 2 * half) + (ad_plus_bc * pow(10, half)) + bd;
+        return ac * powerOfTen(2 * half) + (ad_plus_bc * multiplier) + bd;
     }
 };
 
 int main() {
     Solution solution = Solution();
-    cout << solution.multiply(200, 8) << endl;
+    long long x;
+    long long y;
+    cout << "Enter two numbers: " << endl;
+    cin >> x >> y;
     return 0;
 }
