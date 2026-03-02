9b2904fdd8118932f736318c0c7df69a1fdf2414
qburi <divyeshbalu@gmail.com>
2026-03-03 00:12:32 +0530

    Add ll instead of long long

--- Lab 1/KaratsubaFasterIntegerMultiplicationAlgorithm.cpp
+++ Lab 1/KaratsubaFasterIntegerMultiplicationAlgorithm.cpp
@@ -3,40 +3,41 @@
 
 using namespace std;
 
+#define ll long long
 
 class Solution {
 public:
-    long long powerOfTen(long long n) {
-        long long res = 1;
+    ll powerOfTen(ll n) {
+        ll res = 1;
         for (int i = 0; i < n; i++) {
             res *= 10;
         }
         return res;
     }
 
-    long long multiply(long long x, long long y) {
+    ll multiply(ll x, ll y) {
         if (x < 10 || y < 10)
             return x * y;
 
-        long long n = max(to_string(abs(x)).length(), to_string(abs(y)).length());
-        long long half = n / 2;
-        long long multiplier = powerOfTen(half);
-        long long a = x / multiplier;
-        long long b = x % multiplier;
-        long long c = y / multiplier;
-        long long d = y % multiplier;
+        ll n = max(to_string(abs(x)).length(), to_string(abs(y)).length());
+        ll half = n / 2;
+        ll multiplier = powerOfTen(half);
+        ll a = x / multiplier;
+        ll b = x % multiplier;
+        ll c = y / multiplier;
+        ll d = y % multiplier;
 
-        long long ac = multiply(a, c);
-        long long bd = multiply(b, d);
-        long long ad_plus_bc = multiply(a + b, c + d) - ac - bd;
+        ll ac = multiply(a, c);
+        ll bd = multiply(b, d);
+        ll ad_plus_bc = multiply(a + b, c + d) - ac - bd;
         return ac * powerOfTen(2 * half) + (ad_plus_bc * multiplier) + bd;
     }
 };
 
 int main() {
     Solution solution = Solution();
-    long long x;
-    long long y;
+    ll x;
+    ll y;
     cout << "Enter two numbers: " << endl;
     cin >> x >> y;
     cout << solution.multiply(x, y) << endl;
